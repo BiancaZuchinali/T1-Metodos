dc3ff92d529b4009f866b5fb38b8d2d8c05ec7ca
Bianca Zuchinali <107509260+BiancaZuchinali@users.noreply.github.com>
2024-03-12 00:15:46 -0300

    Update Trabalho_1.c

--- Trabalho_1.c
+++ Trabalho_1.c
@@ -11,7 +11,7 @@ int main() {
   feraiseexcept(FE_DIVBYZERO | FE_UNDERFLOW | FE_OVERFLOW | FE_INEXACT | FE_INVALID);
   feclearexcept(FE_DIVBYZERO | FE_UNDERFLOW | FE_OVERFLOW | FE_INEXACT | FE_INVALID);
   
-  printf("Digite a expressão no formato 'val1 op val2', separando os valores e a operação por espaços.Caso a operação seja multiplicação, use o 'X' como indicador da operação:");
+  printf("Digite a expressão no formato 'val1 op val2', separando os valores e a operação por espaços.Caso a operação seja multiplicação, use o 'X' como indicador da operação: ");
   scanf("%f %c %f", &val1, &op, &val2);
   // Realizando as operações
   switch (op) {
@@ -34,15 +34,15 @@ int main() {
     IEEEval2[i] = (*ponteiro_val2 >> (31 - i)) & 1 ? '1' : '0';
     IEEEr[i] = (*ponteiro_resultado >> (31 - i)) & 1 ? '1' : '0';
   }
-  // Adicionando o caractere nulo ao final dos arrays 
+  // Adicionando o caractere nulo ao final dos arrays
   IEEEval1[32] = '\0';
   IEEEval2[32] = '\0';
   IEEEr[32] = '\0';
-    
+
   printf("Recebi %.6f %c %.6f e o resultado deu %.6f\n\n", val1, op, val2, resultado);
-  printf("Bits de Val1: %c %.8s %.23s = %.0f\n", IEEEval1[0], IEEEval1 + 1, IEEEval1 + 9, val1);
-  printf("Bits de Val2: %c %.8s %.23s = %.0f\n", IEEEval2[0], IEEEval2 + 1, IEEEval2 + 9, val2);
-  printf("Bits do Resultado: %c %.8s %.23s = %.0f\n\n", IEEEr[0], IEEEr + 1, IEEEr + 9, resultado);
+  printf("Bits de Val1: %c %.8s %.23s = %.3f\n", IEEEval1[0], IEEEval1 + 1, IEEEval1 + 9, val1);
+  printf("Bits de Val2: %c %.8s %.23s = %.3f\n", IEEEval2[0], IEEEval2 + 1, IEEEval2 + 9, val2);
+  printf("Bits do Resultado: %c %.8s %.23s = %.3f\n\n", IEEEr[0], IEEEr + 1, IEEEr + 9, resultado);
   printf("Exeção FE_INEXACT: %d\n",  0 != fetestexcept(FE_INEXACT));
   printf("Exeção FE_DIVBYZERO: %d\n", 0 != fetestexcept(FE_DIVBYZERO));
   printf("Exeção FE_UNDERFLOW: %d\n",  0 != fetestexcept(FE_UNDERFLOW));
